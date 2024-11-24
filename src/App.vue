dd38fbd822bca8283e4d7b5767bea6620b3a121d
Alvaro <alvarodurantorrejon@gmail.com>
2024-11-24 17:56:23 +0100

    Vue and bootstrap added

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>{{ message }}</h1>
+  <button @click="showAlert" class="btn btn-primary">press me</button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: "Hello World"
+      }
+    },
+    methods: {
+      showAlert() {
+        alert("Hello World");
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
