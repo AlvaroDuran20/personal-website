b68e0141cc3b8a9d2618cb55173fd9fa3a9d8283
Alvaro <alvarodurantorrejon@gmail.com>
2024-11-27 12:01:26 +0100

    download pdf doesn't work, needs to be fixed

--- src/components/Contact/ContactView.vue
+++ src/components/Contact/ContactView.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>Contact View</h1>
+    <h2>CV</h2>
+    <p>Take a look at my CV!</p>
+    <a href="/public/personalCv/cvLuisDuran.pdf" download="cvLuisDuran.pdf" class="btn btn-primary" role="button">
+      Download My CV
+    </a>
 </template>
 <script>
 export default {
